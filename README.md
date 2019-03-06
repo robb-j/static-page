36e1fffc46927cc2b266bd978e6073b1230ff694
Rob Anderson <rob.anderson@ncl.ac.uk>
2019-03-06 16:41:34 +0000

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,110 @@
+# Static Page
+
+For when you want to quickly throw up a not-so-terrible-looking html page
+and you only want to write markdown.
+This repo is a [node.js](https://nodejs.org) app which compiles assets with
+[unified](https://www.npmjs.com/package/unified),
+[remark](https://www.npmjs.com/package/remark) and
+[rehype](https://www.npmjs.com/package/rehype), styles them with
+[bulma](https://www.npmjs.com/package/bulma) then deploys them with
+[docker](https://www.docker.com/).
+
+## Table of contents
+
+<!-- toc-head -->
+
+## Table of Contents
+
+- [Table of contents](#table-of-contents)
+- [An example](#an-example)
+- [Development](#development)
+  - [Setup](#setup)
+  - [Commands](#commands)
+  - [Code formatting](#code-formatting)
+- [Deployment](#deployment)
+  - [Building the image](#building-the-image)
+
+<!-- toc-tail -->
+
+## An example
+
+Say you want a quick holding page and you have a markdown file with content in, `content.md`.
+
+```md
+---
+title: Coming Soon
+subtitle: You'll have to wait a little bit longer
+theme: #6b2636
+---
+
+# Coming Soon
+
+Etiam porta sem malesuada magna mollis euismod. Etiam porta sem malesuada magna mollis euismod. Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
+```
+
+Then you want to quickly deploy it using docker:
+
+```bash
+docker run -it --rm \
+  -v `pwd`/content.md:/app/page.md
+  -p 3000:3000
+  robb-j/static-page
+```
+
+Then all you have to do is visit `http://localhost:3000`
+
+## Development
+
+### Setup
+
+To develop on this repo you will need to have [Docker](https://www.docker.com/) and
+[node.js](https://nodejs.org) installed on your dev machine and have an understanding of them.
+This guide assumes you have the repo checked out and are on macOS, but equivalent commands are available.
+
+### Commands
+
+```bash
+# Install npm packages
+npm i
+
+# Run the development server
+# -> It will compile the html & css and server them at localhost:3000
+# -> It watches for file changes using `nodemon` and restarts the server
+npm run dev
+
+# Generate the table of contents for this readme
+npm run gen-readme-toc
+```
+
+### Code formatting
+
+This repo uses [Prettier](https://prettier.io/) to automatically format code to a consistent standard.
+It works using the [husky](https://www.npmjs.com/package/husky)
+and [lint-staged](https://www.npmjs.com/package/lint-staged) packages to
+automatically format code whenever code is commited.
+This means that code that is pushed to the repo is always formatted to a consistent standard.
+
+You can manually run the formatter with `npm run prettier` if you want.
+
+Prettier is slightly configured in [.prettierrc.yml](/.prettierrc.yml)
+and also ignores files using [.prettierignore](/.prettierignore).
+
+## Deployment
+
+### Building the image
+
+This repo uses an npm `postversion` script to build, tag and push the docker image to dockerhub.
+Use the `npm version` command to create a new version and it will build and
+push a new docker image with that version.
+This measn that all images are [semantically versioned](https://semver.org/).
+The `:latest` docker tag is not used.
+
+```bash
+# Deploy a new version of the CLI
+npm version # major | minor | patch
+git push --tags
+```
+
+---
+
+> This repo was setup with [robb-j/node-base](https://github.com/robb-j/node-base)
---
+++ src/server.js
@@ -0,0 +1,181 @@
+const { join } = require('path')
+const fs = require('fs')
+const { promisify } = require('util')
+const { createServer } = require('http')
+
+const unified = require('unified')
+const vfile = require('to-vfile')
+const h = require('hastscript')
+const parseMarkdown = require('remark-parse')
+const addToc = require('remark-toc')
+const mdToHtml = require('remark-rehype')
+const frontmatter = require('remark-frontmatter')
+const toHtmlString = require('rehype-stringify')
+const wrapInHtmlDoc = require('rehype-document')
+
+const Yaml = require('yaml')
+const Sass = require('sass')
+
+const { formatMilliseconds } = require('format-ms')
+
+const readFile = promisify(fs.readFile)
+
+const resolvePath = (...paths) => join(__dirname, ...paths)
+
+/** Search a hastscript tree to find a node which matches a predicate */
+function find(node, predicate) {
+  if (!node.children) return
+  for (let child of node.children) {
+    if (predicate(child)) return child
+
+    let nested = find(child, predicate)
+    if (nested) return nested
+  }
+  return null
+}
+
+// A plugin to parse and validate yaml frontmatter
+// -> Should be added after `remark-frontmatter`
+const extractFrontmatter = () => (node, file) => {
+  try {
+    if (node.children[0].type !== 'yaml') {
+      throw new Error('Frontmatter is missing')
+    }
+    let yaml = Yaml.parse(node.children[0].value)
+
+    if (typeof yaml.title !== 'string') throw new Error('No title')
+
+    file.data.matter = yaml
+  } catch (error) {
+    file.fail(error)
+  }
+}
+
+// A plugin to wrap the markdown's content in a bulma page
+const injectPageStructure = () => (node, file) => {
+  const content = node.children
+
+  const { title, subtitle = '' } = file.data.matter
+
+  node.children = [
+    h('.page', [
+      h('.hero.is-primary', [
+        h('.hero-body', [
+          h('.container', [
+            h('h1.title', title),
+            subtitle && h('h2.subtitle', subtitle)
+          ])
+        ])
+      ]),
+      h('section.section.page-expand', [
+        h('.container', [h('.content', [content])])
+      ]),
+      h('footer.footer.has-text-centered.has-text-monospace', [
+        h('.container', [
+          'Page rendered with ',
+          h(
+            'a',
+            { href: 'https://github.com/robb-j/static-page' },
+            'robb-j/static-page'
+          )
+        ])
+      ])
+    ])
+  ]
+}
+
+// A plugin to update `rehype-document`'s title with a value from the frontmatter
+const updateDocumentTitle = () => (node, file) => {
+  let title = find(node, node => node.tagName === 'title')
+  if (title && title.children[0]) {
+    title.children[0].value = file.data.matter.title
+  }
+}
+
+/** Render a markdown file to html, extracting yaml and wrapping in a bulma page */
+function renderMarkdown(file) {
+  let start = Date.now()
+  return new Promise((resolve, reject) =>
+    unified()
+      .use(parseMarkdown)
+      .use(frontmatter, ['yaml'])
+      .use(extractFrontmatter)
+      .use(addToc)
+      .use(mdToHtml)
+      .use(injectPageStructure)
+      .use(wrapInHtmlDoc, { css: ['/style.css'] })
+      .use(updateDocumentTitle)
+      .use(toHtmlString)
+      .process(file, (err, file) => {
+        let duration = Date.now() - start
+
+        if (err) reject(err)
+        else resolve([file.data.matter, file.toString(), duration])
+      })
+  )
+}
+
+/** Compile sass from a file, prepending $primary from yaml frontmatter */
+async function compileSass(file, config) {
+  const { theme = '#3943b7' } = config
+  const data = `$primary: ${theme}\n` + (await readFile(file, 'utf8'))
+
+  const options = {
+    data,
+    indentedSyntax: true,
+    includePaths: [resolvePath('../node_modules')]
+  }
+
+  return new Promise((resolve, reject) => {
+    Sass.render(options, (err, result) => {
+      if (err) reject(err)
+      else resolve([String(result.css), result.stats.duration])
+    })
+  })
+}
+
+;(async () => {
+  try {
+    // Load the markdown page as a virtual file
+    let page = vfile.readSync(resolvePath('../page.md'))
+
+    // Render markdown to html
+    process.stdout.write('Rendering html')
+    const [config, html, t1] = await renderMarkdown(page, frontmatter)
+    process.stdout.write(` (${formatMilliseconds(t1)})\n`)
+
+    // Compile sass into css
+    process.stdout.write('Compilling sass')
+    let [css, t2] = await compileSass(resolvePath('styles.sass'), config)
+    process.stdout.write(` (${formatMilliseconds(t2)})\n`)
+
+    // Create a http server to serve the files
+    let server = createServer((req, res) => {
+      switch (req.url) {
+        case '/':
+        case '/index':
+        case '/index.html': {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'text/html')
+          return res.end(html)
+        }
+        case '/style.css': {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'text/css')
+          return res.end(css)
+        }
+        default: {
+          res.statusCode = '404'
+          res.setHeader('Content-Type', 'text/plain')
+          res.end('Not Found\n')
+        }
+      }
+    })
+
+    // Start the server and listen for requests
+    await new Promise(resolve => server.listen(3000, resolve))
+    console.log('Listening on :3000')
+  } catch (error) {
+    console.log(error)
+  }
+})()
